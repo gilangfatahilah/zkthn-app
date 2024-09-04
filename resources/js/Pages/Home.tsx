e0e6b902c8b007f9b16141d33653991724784b1f
Gilang Fatahilah <gilangf010@gmail.com>
2024-09-05 01:59:36 +0700

    ✨ feat(home): add several page

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -4,6 +4,9 @@ import Navbar from '@/Components/Navbar';
 import Hero from '@/Components/Hero';
 import { ThemeProvider } from '@/Components/themeProvider';
 import Features from '@/Components/Features';
+import HomeCampaign from '@/Components/HomeCampaign';
+import HomeBanner from '@/Components/HomeBanner';
+import Footer from '@/Components/Footer';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
 
@@ -13,6 +16,9 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
             <Navbar user={auth.user} />
             <Hero />
             <Features />
+            <HomeCampaign />
+            <HomeBanner />
+            <Footer />
         </ThemeProvider>
     );
 }
--- resources/js/constans/index.tsx
+++ resources/js/constans/index.tsx
@@ -5,19 +5,19 @@ import { FaHandsHelping } from "react-icons/fa";
 export const features = [
   {
     id: 1,
-    icon: <FaHandHoldingHeart className="w-12 h-12 text-foreground/90" />,
+    icon: <FaHandHoldingHeart className="w-12 h-12 text-white/90" />,
     title: "High Quality",
     description: "Experience the best quality service with cutting-edge technology and top-notch features."
   },
   {
     id: 2,
-    icon: <GiThreeFriends className="w-12 h-12 text-foreground/90" />,
+    icon: <GiThreeFriends className="w-12 h-12 text-white/90" />,
     title: "Responsive Design",
     description: "Our designs are fully responsive, ensuring seamless user experience across all devices."
   },
   {
     id: 3,
-    icon: <FaHandsHelping className="w-12 h-12 text-foreground/90" />,
+    icon: <FaHandsHelping className="w-12 h-12 text-white/90" />,
     title: "Clean Code",
     description: "We write clean, well-documented code to ensure maintainability and ease of use."
   },
