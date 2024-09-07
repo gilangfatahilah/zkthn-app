373b26146df8906ccc08206b1afae46713eb8c75
Muhammad Nur <muhnur7602@gmail.com>
2024-09-07 21:38:37 +0700

    activityDetail

--- app/Http/Controllers/ActivityDetailController.php
+++ app/Http/Controllers/ActivityDetailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use App\Models\ActivityDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -25,6 +26,24 @@ class ActivityDetailController extends Controller
         $detail->user_id = $user->id;
         $detail->status = 1;
 
+        $detail->save();
+
+        return redirect()->back();
+    }
+    public function handleapplier(Request $request)
+    {
+        $detail = ActivityDetail::findOrFail($request->id);
+
+        if ($request->status == 2) {
+            $detail->status = $request->status; // Ambil status dari request
+        }
+
+        if ($request->status == 3) {
+            $detail->status = $request->status; // Ambil status dari request
+        }
+        // Ubah status sesuai dengan data dari request
+
+        // Simpan perubahan
         $detail->save();
 
         return redirect()->back();
