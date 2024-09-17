5163dc3ad7b40b4e479386485b37abbeb07c725b
Muhammad Nur <muhnur7602@gmail.com>
2024-09-17 22:05:36 +0700

    auapaan

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,18 @@
 import AuthenticatedLayout from "@/Layouts/DashboardLayout";
 import { Activity, PageProps } from "@/types";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/Components/ui/card";
-import { CalendarCheck2, CalendarClock, CalendarDays, CreditCard } from "lucide-react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/Components/ui/card";
+import {
+    CalendarCheck2,
+    CalendarClock,
+    CalendarDays,
+    CreditCard,
+} from "lucide-react";
 import { useEffect } from "react";
 import { formatDate } from "@/lib/formatter";
 import { Badge } from "@/Components/ui/badge";
@@ -9,53 +20,69 @@ import { ScrollArea } from "@/Components/ui/scroll-area";
 
 const BadgeStatus = ({ status }: { status: number }) => {
     return (
-        <Badge className={`bg-${status === 1 ? 'yellow' : status === 3 ? 'red' : 'green'}-300`}>
-            {status === 1 ? 'Menunggu' : status === 3 ? 'Ditolak' : 'Diterima'}
+        <Badge
+            className={`bg-${
+                status === 1 ? "yellow" : status === 3 ? "red" : "green"
+            }-300`}
+        >
+            {status === 1 ? "Menunggu" : status === 3 ? "Ditolak" : "Diterima"}
         </Badge>
-    )
-}
+    );
+};
 
 const ActivityCard = ({ activity }: { activity: Activity[] }) => {
-    return (
-        activity.map((a) => {
-            return (<Card key={a.id}>
+    return activity.map((a) => {
+        return (
+            <Card key={a.id}>
                 <CardHeader>
                     <CardTitle>{a.title}</CardTitle>
-                    <CardDescription>Penyelenggara : {a.publised_name}
+                    <CardDescription>
+                        Penyelenggara : {a.publised_name}
                         <br />
                         Jadwal : {formatDate(a.schedule)}
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
                     <div className="flex justify-between">
-                        <img src={`/images/${a.banner}`} alt="banner" className="max-w-32 max-h-32 rounded-lg" />
+                        <img
+                            src={`/images/${a.banner}`}
+                            alt="banner"
+                            className="max-w-48 max-h-48 rounded-lg"
+                        />
 
                         <div className="flex gap-2 items-center">
-
-                            <p className="text-lg">
-                                Status :
-                            </p>
+                            <p className="text-lg">Status :</p>
 
                             <BadgeStatus status={a.status_daftar as number} />
                         </div>
                     </div>
                 </CardContent>
-            </Card>)
-        })
-    )
-}
+            </Card>
+        );
+    });
+};
 
-export default function Dashboard({ auth, activity, pending, accepted, users }: PageProps<{ activity: Activity[], pending?: any, users?: any, accepted?: any }>) {
+export default function Dashboard({
+    auth,
+    activity,
+    pending,
+    accepted,
+    users,
+}: PageProps<{
+    activity: Activity[];
+    pending?: any;
+    users?: any;
+    accepted?: any;
+}>) {
     const role = auth.user.role;
 
     useEffect(() => {
-        console.log(activity)
-    }, [])
+        console.log(activity);
+    }, []);
 
     return (
         <AuthenticatedLayout user={auth.user}>
             <ScrollArea className="h-full">
-
                 <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
                     <h2 className="text-3xl font-bold tracking-tight">
                         Selamat Datang Kembali {auth.user.name ?? ""} 👋
@@ -65,71 +92,70 @@ export default function Dashboard({ auth, activity, pending, accepted, users }: 
                         <Card>
                             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                                 <CardTitle className="text-sm font-medium">
-                                    {
-                                        role === 'personal' ? 'Aktivitas yang diikuti' : 'Total Aktivitas'
-                                    }
+                                    {role === "personal"
+                                        ? "Aktivitas yang diikuti"
+                                        : "Total Aktivitas"}
                                 </CardTitle>
-                                <CalendarDays className='w-4 h-4 text-muted-foreground' />
+                                <CalendarDays className="w-4 h-4 text-muted-foreground" />
                             </CardHeader>
                             <CardContent>
-                                <div className="text-2xl font-bold">{activity.length}</div>
+                                <div className="text-2xl font-bold">
+                                    {activity.length}
+                                </div>
                                 <p className="text-xs text-muted-foreground">
-                                    {
-                                        role === 'personal' ? 'Total aktivitas yang anda ikuti' : 'Total seluruh aktivitas'
-                                    }
+                                    {role === "personal"
+                                        ? "Total aktivitas yang anda ikuti"
+                                        : "Total seluruh aktivitas"}
                                 </p>
                             </CardContent>
                         </Card>
-                        {
-                            role !== 'organization' && (
-                                <Card>
-                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                        <CardTitle className="text-sm font-medium">
-                                            {
-                                                role === 'personal' ? 'Menunggu Konfirmasi' : 'Total Pengguna'
-                                            }
-                                        </CardTitle>
-                                        <CalendarClock className='w-4 h-4 text-muted-foreground' />
-                                    </CardHeader>
-                                    <CardContent>
-                                        <div className="text-2xl font-bold">{role === 'personal' ? pending : users}</div>
-                                        <p className="text-xs text-muted-foreground">
-                                            {role === 'personal' ? 'Menunggu verifikasi dari penyelenggara' : 'Jumlah seluruh pengguna terdaftar'
-                                            }
-                                        </p>
-                                    </CardContent>
-                                </Card>
-                            )
-                        }
+                        {role !== "organization" && (
+                            <Card>
+                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <CardTitle className="text-sm font-medium">
+                                        {role === "personal"
+                                            ? "Menunggu Konfirmasi"
+                                            : "Total Pengguna"}
+                                    </CardTitle>
+                                    <CalendarClock className="w-4 h-4 text-muted-foreground" />
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="text-2xl font-bold">
+                                        {role === "personal" ? pending : users}
+                                    </div>
+                                    <p className="text-xs text-muted-foreground">
+                                        {role === "personal"
+                                            ? "Menunggu verifikasi dari penyelenggara"
+                                            : "Jumlah seluruh pengguna terdaftar"}
+                                    </p>
+                                </CardContent>
+                            </Card>
+                        )}
 
-                        {
-                            role === 'personal' && (
-
-                                <Card>
-                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                        <CardTitle className="text-sm font-medium">
-                                            Diterima
-                                        </CardTitle>
-                                        <CalendarCheck2 className='w-4 h-4 text-muted-foreground' />
-                                    </CardHeader>
-                                    <CardContent>
-                                        <div className="text-2xl font-bold">{accepted}</div>
-                                        <p className="text-xs text-muted-foreground">
-                                            Jumlah aktivitas diterima
-                                        </p>
-                                    </CardContent>
-                                </Card>
-                            )
-                        }
+                        {role === "personal" && (
+                            <Card>
+                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <CardTitle className="text-sm font-medium">
+                                        Diterima
+                                    </CardTitle>
+                                    <CalendarCheck2 className="w-4 h-4 text-muted-foreground" />
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="text-2xl font-bold">
+                                        {accepted}
+                                    </div>
+                                    <p className="text-xs text-muted-foreground">
+                                        Jumlah aktivitas diterima
+                                    </p>
+                                </CardContent>
+                            </Card>
+                        )}
                     </div>
 
-                    {
-                        role === 'personal' && (
-                            <ActivityCard activity={activity} />
-                        )
-                    }
+                    {role === "personal" && (
+                        <ActivityCard activity={activity} />
+                    )}
                 </div>
-
             </ScrollArea>
         </AuthenticatedLayout>
     );
